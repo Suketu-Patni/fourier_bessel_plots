fixed off-by-one error

code1.py
@@ -6,7 +6,7 @@ from functools import lru_cache
 p = 0
 q = 1
 p_prime_range = range(1, 11)
-epsilon_by_r = 0.15 # change this value to get the different plots
+epsilon_by_r = 0.01 # change this value to get the different plots
 u_p_q = ss.jn_zeros(p, q)[-1]
 lower_bound = ss.jv(p + 1, u_p_q)**2 / 4 - epsilon_by_r**2 / 2
 
@@ -21,19 +21,14 @@ def n_term_integrand(p, q, p_prime, n, t):
     
 
 for p_prime in p_prime_range:
-    l = 1
-    u_p_prime_list = ss.jn_zeros(p_prime, 1000)
+
+    l = 1    
+    l_term_sum = 0
 
     while True:
-        l_term_sum = 0
-        integrals = []
-        
-        # vectorize
-        for n in range(1, l):
-            n_term_integral, _ = integrate.quad(lambda t: n_term_integrand(p, q, p_prime, n, t), 0, 1)
-            integrals.append(n_term_integral)
-
-        l_term_sum = np.sum(np.square(integrals))
+    
+        lth_term_integral, _ = integrate.quad(lambda t: n_term_integrand(p, q, p_prime, l, t), 0, 1)
+        l_term_sum += lth_term_integral ** 2
 
         if l_term_sum > lower_bound:
             print(p_prime, l)